42f2b547184d87335c5756fb3df872a79c28d125
jr-xavier <antonionjrxavier@gmail.com>
2026-05-07 01:19:02 -0300

    Correção: Sincronizando urls com a função home da views

--- escola/urls.py
+++ escola/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # Ajustado para 'home' para bater com sua views.py
+    path('', views.home, name='index'), 
+    
+    # Rota para o processamento dos dados
     path('processar_matricula/', views.processar_matricula, name='processar_matricula'),
 ]
