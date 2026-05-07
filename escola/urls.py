d0fa48a72595495cd50de67dc012bad59eda2d72
jr-xavier <antonionjrxavier@gmail.com>
2026-05-07 01:06:06 -0300

    Registrando as novas rotas e views

--- escola/urls.py
+++ escola/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'), # ou views.index, dependendo do nome da sua função
+    path('processar/', views.processar_matricula, name='processar_matricula'),
 ]
