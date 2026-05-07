d22099373cf9591240df2d5bc9b7ca489a0eeb80
jr-xavier <antonionjrxavier@gmail.com>
2026-05-07 01:00:23 -0300

    Criando tela de contrato e finalizando fluxo

--- escola/views.py
+++ escola/views.py
@@ -5,3 +5,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'escola/index.html')
+
+def processar_matricula(request):
+    if request.method == 'POST':
+        context = {
+            'nome_aluno': request.POST.get('nome_aluno'),
+            'data_nascimento': request.POST.get('data_nascimento'),
+            'nome_escola': request.POST.get('nome_escola'), # Adicionado
+            'nome_responsavel': request.POST.get('nome_responsavel'),
+            'documento': request.POST.get('documento'),
+            'telefone_1': request.POST.get('telefone_1'),
+            'telefone_2': request.POST.get('telefone_2'),
+        }
+        return render(request, 'escola/contrato.html', context)
