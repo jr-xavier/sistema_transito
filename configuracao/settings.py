287af81ea8b2ab5e3559de79160fcbb38119270f
jr-xavier <antonionjrxavier@gmail.com>
2026-05-07 20:14:17 -0300

    Correção: Movendo templates para a raiz para validar o login personalizado

--- configuracao/settings.py
+++ configuracao/settings.py
@@ -14,13 +14,14 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
+    'escola',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'escola',
+    
 ]
 
 MIDDLEWARE = [
